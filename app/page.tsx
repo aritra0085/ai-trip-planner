0d1b0b3301da9405d7521446463945b7b8539171
aritra0085 <aritrabiswas0085@gmail.com>
2025-08-11 20:30:56 +0530

    use shadcn and edit it

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>hi</h1>
+      <h1>hi my name is aritra</h1>
+      <Button>aritra</Button>
     </div>
   );
 }
