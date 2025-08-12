474549207f0eeb98f82edccf11a4cc82daee0853
aritra0085 <aritrabiswas0085@gmail.com>
2025-08-12 13:02:58 +0530

    add header and one line of content

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import Link from 'next/link'
 import path from 'path'
@@ -19,21 +20,22 @@ const MenuOptions=[
 ]
 const Header = () => {
   return (
-    <div>
+    <div className='flex justify-between items-center p-4'>
       {/* Logo */}
       <div className='flex gap-2 items-center'>
         <Image src={'/logo.svg'} alt='logo' width={30} height={30}/>
         <h2 className='font-bold text-2xl'> AI Trip Planner</h2>
         </div>
       {/* Menu Options */}
-        <div>
+        <div className='flex gap-8 items-center'>
           {MenuOptions.map((menu,index)=>(
             <Link href={menu.path}>
-            <h2>{menu.name}</h2>
+            <h2 className='text-lg hover:scale-105 transition-all hover:text-primary'>{menu.name}</h2>
             </Link>
           ))}
         </div>
       {/* Get Started Button */}
+      <Button>Get Started</Button>
     </div>
   )
 }
---
+++ app/_components/Hero.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='mt-24 w-full flex justify-center'>
+      {/* Content */}
+      <div className='max-w-3xl w-full text-center'>
+        <h1 className='text-xl md:text-5xl font-bold'>Hey, I'm your personal <span className='text-primary'>Trip Planner</span> </h1>
+      </div>
+      {/* input Box */}
+
+      {/* Suggestion list */}
+
+      {/* video Section */}
+
+    </div>
+  )
+}
+
+export default Hero
