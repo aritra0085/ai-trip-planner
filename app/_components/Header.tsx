c82f5f6cd3a51e40a46a14eee0f5be7bf604d974
aritra0085 <aritrabiswas0085@gmail.com>
2025-08-12 12:02:23 +0530

    add home pricing and contact us

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,13 +1,38 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import path from 'path'
 import React from 'react'
 
+const MenuOptions=[
+  {
+    name:'Home',
+    path:'/'
+  },
+  {
+    name: 'Pricing',
+    path: '/pricing'
+  },
+  {
+    name: 'Contact us',
+    path:'/contact-us'
+  }
+]
 const Header = () => {
   return (
     <div>
       {/* Logo */}
+      <div className='flex gap-2 items-center'>
         <Image src={'/logo.svg'} alt='logo' width={30} height={30}/>
+        <h2 className='font-bold text-2xl'> AI Trip Planner</h2>
+        </div>
       {/* Menu Options */}
-
+        <div>
+          {MenuOptions.map((menu,index)=>(
+            <Link href={menu.path}>
+            <h2>{menu.name}</h2>
+            </Link>
+          ))}
+        </div>
       {/* Get Started Button */}
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>hi my name is aritra</h1>
-      <Button>aritra</Button>
+      
     </div>
   );
 }
