eaf0d29a8d0dd9ab3f649735092d3f0b9a53460c
aritra0085 <aritrabiswas0085@gmail.com>
2025-08-13 23:27:19 +0530

    make database

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import {Geist, Outfit} from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "./ConvexClientProvider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,15 +20,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={outfit.className}
-      >
-        <Provider>
-          {children}
-        </Provider>
-      
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={outfit.className}
+        >
+          <ConvexClientProvider>
+            {children}
+          </ConvexClientProvider>
+        
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
