d1579ccb73bf489f626bfadbffaed5650d10a3d3
aritra0085 <aritrabiswas0085@gmail.com>
2025-08-18 13:09:46 +0530

    add the check box

--- app/create-new-trip/page.tsx
+++ app/create-new-trip/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import ChatBox from './_components/ChatBox'
 
 const CreateNewTrip = () => {
   return (
-    <div>
-      
+    <div className='grid grid-cols-1 md:grid-cols-2 gap-5 p-10'>
+      <div>
+        {/* chatbox */}
+        <ChatBox/>
+      </div>
+      <div>
+        {/* Map and trip plan to Display */}
+      </div>
     </div>
   )
 }
