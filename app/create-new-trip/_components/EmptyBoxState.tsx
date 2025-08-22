4a827daf72d78fdd04566f69186c7ab152fb0353
aritra0085 <aritrabiswas0085@gmail.com>
2025-08-22 17:56:34 +0530

    add paragraph

--- app/create-new-trip/_components/EmptyBoxState.tsx
+++ app/create-new-trip/_components/EmptyBoxState.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 function EmptyBoxState() {
   return (
     <div className='mt-7'>
-      <h2 className='font-bold text-xl text-center'>Start Planning new <strong className='text-primary'>Trip</strong>  using AI</h2>
+      <h2 className='font-bold text-3xl text-center'>Start Planning new <strong className='text-primary'>Trip</strong>  using AI</h2>
+      <p className='text-center'>Discover personalized travel itineraries, find the best destinations, and plan your dream vacation effortlessly with the power of AI. Let our smart assistant do the hard work while you enjoy the journey.</p>
     </div>
   )
 }
